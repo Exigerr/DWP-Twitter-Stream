9b804cce83a8908c54c11f6a0b7930496737bea6
Exigerr <alisterstevens@googlemail.com>
2015-01-28 10:50:40 +0000

    Added Vector Class

--- Client/Scripts/classesAndHelpers.js
+++ Client/Scripts/classesAndHelpers.js
@@ -3,12 +3,109 @@ function translateElem(elem, x, y){
     elem.style.transform = 'translate('+x+'%, '+y+'%)';
 }
 
-
+//Simple Vector
+//---------------------------------------------------------------
 var SimpleVector = (function SimpleVector(x,y){
     this.x = x;
     this.y = y;
 });
 
+
+//Uber fast version of Math.atan2
+//---------------------------------------------------------------
+function atan2(x,y){
+    if (x == 0) {
+        if (y > 0) return Math.PI / 2;
+        if (y == 0) return 0;
+        return -Math.PI / 2;
+    }
+    var radian, z = y / x;
+    if (z < -1 || z > 1) {
+        radian = Math.PI / 2 - z / (z * z + 0.28);
+        if (this.y < 0) return radian - Math.PI;
+    }
+    else {
+        radian = z / (1 + 0.28 * z * z);
+        if (x < 0) {
+            if (y < 0) return radian - Math.PI;
+            return radian + Math.PI;
+        }
+    }
+    return radian;
+}
+
+//Vector Object
+//---------------------------------------------------------------
+var Vector = (function(){
+    function Vector(x,y){
+        this.x = 0;
+        this.y = 0;
+        this.angle = 0.00; //Radian
+    }
+
+    Vector.prototype.normalise = function(){
+        var length = Math.sqrt(this.x * this.x + this.y * this.y);
+        if(length !== 0){
+            this.x /= length;
+            this.y /= length;
+        }
+    };
+
+    Vector.prototype.add = function(pos){
+        this.x += pos.x;
+        this.y += pos.y;
+
+        return this;
+    };
+
+    Vector.prototype.scale = function(n, n2) {
+        this.x *= n;
+        this.y *= n2 || n;
+
+        return this;
+    };
+
+    Vector.prototype.traject = function(speed) {
+        this.x += speed * Math.cos(this.angle);
+        this.y += speed * Math.sin(this.angle);
+    };
+
+    Vector.prototype.copy = function(pos){
+        this.x = pos.x;
+        this.y = pos.y;
+
+        return this;
+    };
+
+    Vector.prototype.subtract = function(pos){
+        this.x -= pos.x;
+        this.y -= pos.y;
+    };
+
+    Vector.prototype.dot = function(pos){
+        return (this.x * pos.x) + (this.y *pos.y);
+    };
+
+    Vector.prototype.cross = function(pos){
+        return (this.x * pos.x) - (this.y * pos.y);
+    };
+
+    Vector.prototype.perpendicular = function() {
+        var temp = this.x;
+        this.x = this.y;
+        this.y = 0-temp;
+    };
+
+    Vector.prototype.normal = function(){
+        this.perpendicular();
+        this.normalise();
+    };
+
+    return Vector;
+})();
+
+//Canvas Handler
+//---------------------------------------------------------------
 var Canvas =  (function () {
     function Canvas (canvas, width, height){
         this.elem = canvas;
