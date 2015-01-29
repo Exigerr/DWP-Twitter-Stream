7c484051225b22064ce66f70e2c304d4430b1270
Exigerr <alisterstevens@googlemail.com>
2015-01-29 12:14:05 +0000

    Update base server to go through controller and handle invalid JSON

--- Server/server.js
+++ Server/server.js
@@ -1,33 +1,22 @@
+//Server Requires and Base Setup
 var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: 1337});
-var OAuth = require('oauth');
+    wss = new WebSocketServer({port: 1337}),
+    config = require('./Config/default.json'),
+    TwitterHandler = require('./Core/twitter.js'),
+    Controller = require('./Core/controller.js');
 
-var config = {
-    consumerKey: 'jRkuxL691D6c0Dll5Qyr9utlY',
-    consumerToken: 'qZLrAcYcMFc3ZspZ6Uy2mBE2ZsYIVv2LG8oOuqlEMek7KW2Uhf',
-    token: '3002890787-ZwgAs4BmD9uLu4K8tM4ckBJV6K69oSoGG1G3o63',
-    tokenSecret: 'o23hO89XSdQcmr7NTx7kjp26EfGQ0nHU9VAlgIi6ygrB3'
-};
-
-var twitterOauth = new OAuth(
-    'https://api.twitter.com/oauth/request_token',
-    'https://api.twitter.com/oauth/access_token',
-    config.consumerKey,
-    config.consumerToken,
-    '1.0A',
-    null,
-    'HMAC-SHA1'
-);
-
+//App Setup
+var controller = new Controller();
+var twitter = new TwitterHandler(config.twitterKeys);
 
 wss.on('connection', function(ws) {
     ws.on('message', function(obj) {
-
-
-    });
-
-    ws.on('close', function(){
-
+        try{
+            controller.route(JSON.parse(obj), ws);
+        }
+        catch(e){
+            console.log('Someone naughty sent invalid JSON! :O');
+        }
 
     });
 });
