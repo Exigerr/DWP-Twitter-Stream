437609fd2dfd7e2a709a57d208457aabe4dd216d
Exigerr <alisterstevens@googlemail.com>
2015-02-01 14:30:52 +0000

    Minor Updates

--- Server/Core/twitter.js
+++ Server/Core/twitter.js
@@ -1,25 +1,45 @@
 var OAuth = require('oauth');
 
-var TwitterHandler =  (function () {
-    function TwitterHandler (config, type){
+var TwitterStream =  (function () {
+    function TwitterStream (config, type){
+        this.config = config;
         this.connection = new OAuth.OAuth(
             'https://api.twitter.com/oauth/request_token',
             'https://api.twitter.com/oauth/access_token',
-            config.consumerKey,
-            config.consumerToken,
+            this.config.consumerKey,
+            this.config.consumerToken,
             '1.0A',
             null,
             'HMAC-SHA1'
         );
+        this.request = null;
         this.url = 'https://stream.twitter.com/1.1/statuses/'+type+'.json';
     }
 
-    TwitterHandler.prototype.stream = function(){
+    TwitterStream.prototype.start = function(params, callback){
+        this.request = this.connection.get(
+            this.url+'?track='+ params,
+            this.config.token,
+            this.config.secret
+        );
+        this.request.on('response', function(response){
+            response.setEncoding("utf8");
+            response.on("data", callback);
+            response.on("end", function () {
+                console.log("---  STREAM ENDED  ---");
+            });
+        });
+        this.request.end();
+        setTimeout(this.stop.bind(this), 5000);
+    };
 
+    TwitterStream.prototype.stop = function(){
+        console.log('---  PREPARE TO STOP!  ---');
+        this.request.abort();
     };
 
 
-    return TwitterHandler;
+    return TwitterStream;
 })();
 
-module.exports = TwitterHandler;
+module.exports = TwitterStream;
--- Server/server.js
+++ Server/server.js
@@ -2,21 +2,26 @@
 var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 1337}),
     config = require('./Config/default.json'),
-    TwitterHandler = require('./Core/twitter.js'),
+    TwitterStream = require('./Core/twitter.js'),
+    THandler = require('./Core/handler.js'),
     Controller = require('./Core/controller.js');
 
 //App Setup
 var controller = new Controller();
-var twitter = new TwitterHandler(config.twitterKeys);
+var twitter = new TwitterStream(config.twitterKeys, 'filter');
+var tHandler = new THandler(twitter);
+twitter.start(tHandler.getParams(), tHandler.response.bind(tHandler));
 
 wss.on('connection', function(ws) {
     ws.on('message', function(obj) {
+        var data;
         try{
-            controller.route(JSON.parse(obj), ws);
+            data = JSON.parse(obj);
         }
         catch(e){
             console.log('Someone naughty sent invalid JSON! :O');
+            return;
         }
-
+        controller.route(data, ws);
     });
 });
